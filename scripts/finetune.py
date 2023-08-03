a8b9b306a2692b715f175a30af22539e1234adb9
Eugene Pentland <eugenepentland@gmail.com>
2023-08-03 21:44:03 +0000

    another

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -199,7 +199,7 @@ def train(
                 cfg[k] = json.loads(str(v))
             print(type(cfg[k]))
     print("Loading the config")
-    pprint(cfg.items())
+    pprint(cfg.keys(), cfg.values())
     pprint(cfg['datasets'], type(cfg['datasets']))
     validate_config(cfg)
 
