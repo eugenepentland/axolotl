097393eda0158cd9cec7fea370cbff8bb44018d5
Eugene Pentland <eugenepentland@gmail.com>
2023-08-03 21:00:37 +0000

    more logging

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -189,7 +189,7 @@ def train(
         # if not strict, allow writing to cfg even if it's not in the yml already
         if k in cfg_keys or not cfg.strict:
             # handle booleans
-            print("casting", v)
+            print("casting",k, "to", v)
             if isinstance(cfg[k], bool):
                 cfg[k] = bool(v)
             elif isinstance(v, str):
