ac1d483e20482d5eb25271df7c1ca78c3aa06083
Eugene Pentland <eugenepentland@gmail.com>
2023-08-05 23:17:51 +0000

    add magic

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -175,8 +175,7 @@ def train(
         config = choose_config(config)
 
     if config == "wandb":
-        wandb.init()
-
+        wandb.init(magic=True)
         run_config = json.loads(os.environ['WANDB_CONFIG'])
         cfg: DictDefault = DictDefault(run_config)
         cfg['use_wandb'] = True
